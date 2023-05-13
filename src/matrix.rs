5c6d24cffc75e11654b1a5503b87e388675daef3
Alexander Fedorovskyi <kikban030@gmail.com>
2023-05-13 17:58:53 +0300

    feat: race conditions

---
+++ src/matrix.rs
@@ -0,0 +1,123 @@
+use std::{
+    io::{stdin, stdout, Write},
+    thread, time,
+};
+
+use rand::Rng;
+
+const ROWS: usize = 100;
+const COLS: usize = 100;
+
+pub fn run() {
+    // Create two empty matrices with random values
+    let matrix1 = generate_matrix();
+    let matrix2 = generate_matrix();
+
+    // Common mult
+    let time = std::time::Instant::now();
+    let _result = multiply_matrices(&matrix1, &matrix2);
+    let elapsed = time.elapsed();
+
+    println!("Time elapsed for common mult: {}ms", elapsed.as_millis());
+
+    // Threaded mult
+    let time = std::time::Instant::now();
+    let _result_threaded = unsafe { multiply_matrices_threaded(&matrix1, &matrix2, true) };
+    let elapsed = time.elapsed();
+
+    println!(
+        "Time elapsed for multithreaded mult: {}ms",
+        elapsed.as_millis()
+    );
+}
+
+fn generate_matrix() -> Vec<Vec<u32>> {
+    let mut matrix = Vec::with_capacity(ROWS);
+    let mut rng = rand::thread_rng();
+    for _i in 0..ROWS {
+        let row = (0..COLS)
+            .map(|_| rng.gen_range::<u32, _>(0..1000))
+            .collect();
+        matrix.push(row);
+    }
+
+    matrix
+}
+
+fn _print_matrix(matrix: &[Vec<u32>]) {
+    for row in matrix {
+        for &val in row {
+            print!("{:8} ", val);
+        }
+        println!();
+    }
+}
+
+fn multiply_matrices(matrix1: &[Vec<u32>], matrix2: &[Vec<u32>]) -> Vec<Vec<u32>> {
+    let mut product = Vec::with_capacity(ROWS);
+    for _i in 0..ROWS {
+        let row = vec![0; COLS];
+        product.push(row);
+    }
+
+    for i in 0..ROWS {
+        for j in 0..COLS {
+            for k in 0..COLS {
+                product[i][j] += matrix1[i][k] * matrix2[k][j];
+            }
+        }
+    }
+
+    product
+}
+
+unsafe fn multiply_matrices_threaded(
+    matrix1: &[Vec<u32>],
+    matrix2: &[Vec<u32>],
+    print: bool,
+) -> Vec<Vec<u32>> {
+    struct ResultPtr(*mut u32);
+    impl ResultPtr {
+        unsafe fn set(&self, value: u32) {
+            *self.0 = value;
+        }
+    }
+    unsafe impl Send for ResultPtr {}
+    unsafe impl Sync for ResultPtr {}
+
+    let mut result = Vec::with_capacity(ROWS);
+    for _i in 0..ROWS {
+        let row = vec![0; COLS];
+        result.push(row);
+    }
+
+    let mut handles = Vec::with_capacity(ROWS * COLS);
+
+    for i in 0..ROWS {
+        for j in 0..COLS {
+            let matrix1_row = matrix1[i].clone();
+            let matrix2_col = matrix2.iter().map(|row| row[j]).collect::<Vec<u32>>();
+            let result_ptr = ResultPtr(&mut result[i][j] as *mut u32);
+
+            let handle = unsafe {
+                thread::spawn(move || {
+                    let mut sum = 0;
+                    for k in 0..COLS {
+                        sum += matrix1_row[k] * matrix2_col[k];
+                    }
+                    if print {
+                        println!("[{}, {}] = {}", i, j, sum);
+                    }
+                    result_ptr.set(sum);
+                })
+            };
+            handles.push(handle);
+        }
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    result
+}
---
+++ src/race.rs
@@ -0,0 +1,116 @@
+use std::{
+    io::{stdin, stdout, Write},
+    println,
+    sync::{
+        atomic::{AtomicU32, Ordering},
+        Arc, Mutex,
+    },
+    thread, time,
+};
+
+#[derive(Clone)]
+struct RawPtr(*mut u32);
+impl RawPtr {
+    unsafe fn add(&self, value: u32) {
+        *self.0 += value;
+    }
+}
+unsafe impl Send for RawPtr {}
+unsafe impl Sync for RawPtr {}
+
+const REPEATS: usize = 1000000;
+
+pub fn run() {
+    let mut time = time::Instant::now();
+    without_sync();
+    println!(
+        "Time elapsed for without sync: {}ms\n",
+        time.elapsed().as_millis()
+    );
+
+    time = time::Instant::now();
+    with_mutex();
+    println!(
+        "Time elapsed for with mutex: {}ms\n",
+        time.elapsed().as_millis()
+    );
+
+    time = time::Instant::now();
+    with_atomic();
+    println!(
+        "Time elapsed for with atomic: {}ms\n",
+        time.elapsed().as_millis()
+    );
+}
+
+fn without_sync() {
+    let mut v = 0u32;
+    let ptr1 = RawPtr(&mut v as *mut u32);
+    let ptr2 = ptr1.clone();
+
+    let t1 = thread::spawn(move || unsafe {
+        for _ in 0..REPEATS {
+            ptr1.add(1);
+        }
+    });
+
+    let t2 = thread::spawn(move || unsafe {
+        for _ in 0..REPEATS {
+            ptr2.add(1);
+        }
+    });
+
+    t1.join().unwrap();
+    t2.join().unwrap();
+
+    println!("Without sync:");
+    println!("\tv = {}", v);
+}
+
+fn with_mutex() {
+    let v1 = Arc::new(Mutex::new(0u32));
+    let v2 = v1.clone();
+    let v = v1.clone();
+
+    let t1 = thread::spawn(move || {
+        for _ in 0..REPEATS {
+            *v1.lock().unwrap() += 1
+        }
+    });
+
+    let t2 = thread::spawn(move || {
+        for _ in 0..REPEATS {
+            *v2.lock().unwrap() += 1
+        }
+    });
+
+    t1.join().unwrap();
+    t2.join().unwrap();
+
+    println!("With mutex:");
+    println!("\tv = {}", v.lock().unwrap());
+}
+
+fn with_atomic() {
+    let v1 = Arc::new(AtomicU32::new(0u32));
+    let v2 = v1.clone();
+    let v = v1.clone();
+
+    let t1 = thread::spawn(move || {
+        for _ in 0..REPEATS {
+            v1.fetch_add(1, Ordering::Relaxed);
+        }
+    });
+
+    let t2 = thread::spawn(move || {
+        for _ in 0..REPEATS {
+            v2.fetch_add(1, Ordering::Relaxed);
+        }
+    });
+
+    t1.join().unwrap();
+    t2.join().unwrap();
+
+    println!("With atomic:");
+    println!("\tv = {}", v.load(Ordering::Relaxed));
+}
